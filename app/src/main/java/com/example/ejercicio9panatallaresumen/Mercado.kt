602317b8b78da16489646e9c258b0f06f9f97fb8
Marcosher249 <marher249@alu.centroafuera.es>
2023-02-15 09:15:04 +0100

    Actualizacion2

--- app/src/main/java/com/example/ejercicio9panatallaresumen/Mercado.kt
+++ app/src/main/java/com/example/ejercicio9panatallaresumen/Mercado.kt
@@ -3,22 +3,69 @@ package com.example.ejercicio9panatallaresumen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.view.isVisible
 import com.example.ejercicio9panatallaresumen.databinding.ActivityMainBinding
 import com.example.ejercicio9panatallaresumen.databinding.ActivityMercadoBinding
 
 class Mercado : AppCompatActivity() {
     lateinit var binding : ActivityMercadoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMercadoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button20.setOnClickListener{
+        var cantidad = 0
+        val precio = binding.textView15.text.toString().toInt()
+        binding.continuar.setOnClickListener{
             val intent = Intent(this, Dado::class.java)
             startActivity(intent)
         }
-        binding.button19.setOnClickListener{
-            val intent = Intent(this, ContinueMercado::class.java)
-            startActivity(intent)
+        binding.comerciar.setOnClickListener{
+            binding.comerciar.isVisible = false
+            binding.continuar.isVisible = false
+            binding.comprar.isVisible = true
+            binding.vemder.isVisible = true
+            binding.cancelar.isVisible = true
         }
+        binding.cancelar.setOnClickListener{
+            binding.comerciar.isVisible = true
+            binding.continuar.isVisible = true
+            binding.comprar.isVisible = false
+            binding.vemder.isVisible = false
+            binding.cancelar.isVisible = false
+        }
+        binding.comprar.setOnClickListener {
+            binding.imageView9.setImageResource(R.drawable.objeto)
+            binding.mas.isVisible = true
+            binding.menos.isVisible = true
+            binding.textView12.isVisible = true
+
+        }
+        binding.mas.setOnClickListener {
+            if (cantidad < 100) {
+                binding.textView12.text = (cantidad + 1).toString()
+                cantidad = (binding.textView12.text).toString().toInt()
+                binding.textView17.text = (cantidad * precio).toString()
+            }
+        }
+        binding.menos.setOnClickListener{
+            if (cantidad>0){
+                binding.textView12.text = (cantidad - 1).toString()
+                cantidad = (binding.textView12.text).toString().toInt()
+                binding.textView17.text = (cantidad * precio).toString()
+            }
+
+        }
+        binding.vemder.setOnClickListener {
+            binding.imageView9.setImageResource(R.drawable.esponja)
+        }
+
+
+
+
+
+
     }
 }
